749f0ac91fbd2cc6bc7c9f13fb4264f6e0b8ec38
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-10-10 00:58:33 +0200

    sc

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,16 +8,16 @@ export default function Footer() {
     return (
         <div className="row">
             <Col md={2}>
-                <img src="/img/react.png" width="100%"/>
+                <img src="/img/react.png" width="100%" alt="1"/>
             </Col>
             <Col md={2}>
-                <img src="/img/bootstrap.png" width="100%"/>
+                <img src="/img/bootstrap.png" width="100%"  alt="2"/>
             </Col>
             <Col md={2}>
-                <img src="/img/api_rest.png" width="100%"/>
+                <img src="/img/api_rest.png" width="100%"  alt="3"/>
             </Col>
             <Col md={2}>
-                <img src="/img/reactstrap.png" width="100%"/>
+                <img src="/img/reactstrap.png" width="100%"  alt="4"/>
             </Col>
         </div>
     )
